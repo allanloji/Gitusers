e4305cbf787e040ad2e00ba6d4e1fb095f25c2cb
Allan Lopez <n16h7hun73r@gmail.com>
2021-08-15 13:42:57 -0500

    :white_check_mark: Add Select test

--- components/Select/__tests__/Select.test.tsx
+++ components/Select/__tests__/Select.test.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import Select from '..';
 
 describe('Select', () => {
-  test('renders successfully', () => {
-    render(<Select />);
-    const element = screen.getByTestId(/test/i);
-    expect(element).toBeInTheDocument();
+  test('Should render', () => {
+    const onChange = jest.fn();
+    const options = [
+      { label: 'Any order', value: '' },
+      { label: 'Asc', value: 'asc' },
+      { label: 'Desc', value: 'desc' },
+    ];
+
+    const { container } = render(<Select name='perPage' onChange={onChange} options={options} />);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  test('Should execute onChange function', () => {
+    const onChange = jest.fn();
+    const options = [
+      { label: 'Any order', value: '' },
+      { label: 'Asc', value: 'asc' },
+      { label: 'Desc', value: 'desc' },
+    ];
+
+    const { getByRole } = render(<Select name='perPage' onChange={onChange} options={options} />);
+
+    const input = getByRole('combobox');
+    userEvent.selectOptions(input, 'Asc');
+
+    expect(onChange).toHaveBeenCalledWith('asc');
   });
 });
